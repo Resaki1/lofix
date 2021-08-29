67105631ba06e727ef23df10fc9c0129cfe8c6bb
Chris' MBP <christian.reski@t-online.de>
2021-08-29 19:58:26 +0200

    requesting movie details from the movie database

--- src/App.tsx
+++ src/App.tsx
@@ -14,22 +14,33 @@ function App() {
 
   const handleClick = async () => {
     // open file picker
-    const dirHandle = await (window as any).showDirectoryPicker({ multiple: true });
+    const dirHandle = await (window as any).showDirectoryPicker({ multiple: true })
     await set("directory", dirHandle);
 
     let entries = [];
     // loop over all entries in directory
     for await (const fileHandle of dirHandle.values()) {
-      //check if entry is a video file
+      // check if entry is a video file
       if (fileHandle.kind === "file") {
         const file = await fileHandle.getFile()
         if (file.type === "video/mp4") {
-          const movie = {
-            name: fileHandle.name,
-            fileHandle: fileHandle,
+          const nameToSearch = fileHandle.name.replaceAll(" ", "+").replaceAll("_", "+").split('.').slice(0, -1).join('.')
+
+          // request movie details from tmdb
+          const response = fetch(`https://api.themoviedb.org/3/search/movie?api_key=da2b03ca0b0a10e22f32080f94056b75&query=${nameToSearch}`)
+            .then(res => res.json())
+            .catch(error => console.log(error))
+
+          const searchResult = await response;
+
+          if (searchResult.results.length > 0) {
+            const movie = {
+              name: searchResult.results[0].original_title,
+              fileHandle: fileHandle,
+            }
+            await set(movie.name, movie)
+            entries.push(movie)
           }
-          await set(movie.name, movie)
-          entries.push(movie)
         }
       }
     }
