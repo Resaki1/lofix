1029106f159b135a300994b47e3c499f646a5993
Resaki <christian.reski@t-online.de>
2021-09-09 19:52:13 +0200

    implemented more reliant way to get posters and backdrops

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { values, set } from 'idb-keyval';
-import { getMovieDetails, getMovieBackdrop, getMoviePoster } from "./api/api"
+import React from "react";
+import { values, set } from "idb-keyval";
+import { getMovieDetails, getImage } from "./api/api";
 import Popup from "./components/Popup/Popup";
-import './App.css';
-import MovieCard from './components/MovieCard/MovieCard';
+import "./App.css";
+import MovieCard from "./components/MovieCard/MovieCard";
 
 function App() {
-  const [movies, setMovies]: any[] = React.useState()
-  const [loading, setLoading]: any[] = React.useState(false)
+  const [movies, setMovies]: any[] = React.useState();
+  const [loading, setLoading]: any[] = React.useState(false);
 
   const handleClick = async () => {
     // open file picker
-    const dirHandle = await (window as any).showDirectoryPicker({ multiple: true })
-    setLoading(true)
+    const dirHandle = await (window as any).showDirectoryPicker({
+      multiple: true,
+    });
+    setLoading(true);
     await set("directory", dirHandle);
 
     let entries = [];
@@ -20,45 +22,47 @@ function App() {
     for await (const fileHandle of dirHandle.values()) {
       // check if entry is a video file
       if (fileHandle.kind === "file") {
-        const file = await fileHandle.getFile()
+        const file = await fileHandle.getFile();
         if (file.type === "video/mp4") {
           const searchResult = await getMovieDetails(fileHandle.name);
+
+          console.log(fileHandle.name + ":");
+          console.log(searchResult);
 
           if (searchResult.results.length > 0) {
             const movie = {
               id: searchResult.results[0].id,
               name: searchResult.results[0].original_title,
-              poster: await getMoviePoster(searchResult.results[0].id),
-              backdrop: await getMovieBackdrop(searchResult.results[0].id),
+              poster: await getImage(searchResult.results[0].poster_path),
+              backdrop: await getImage(searchResult.results[0].backdrop_path),
               fileHandle: fileHandle,
-            }
-            await set(movie.id, movie)
-            entries.push(movie)
+            };
+            await set(movie.id, movie);
+            entries.push(movie);
           }
         }
       }
     }
-    setLoading(false)
+    setLoading(false);
     // TODO: add movies one by one for better loading experience
-    setMovies(entries)
-  }
+    setMovies(entries);
+  };
 
   React.useEffect(() => {
     values().then((values) => {
-      setMovies(values)
-    })
-  }, [])
+      setMovies(values);
+    });
+  }, []);
 
   return (
     <div className="App">
       <button onClick={handleClick}>choose directory</button>
       <div className="movies">
-        {
-          loading && <h1>loading</h1>
-        }
-        {
-          movies && movies.map((movie: any) => <MovieCard key={movie.id} movie={movie} />)
-        }
+        {loading && <h1>loading</h1>}
+        {movies &&
+          movies.map((movie: any) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
       </div>
     </div>
   );
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,50 +1,74 @@
-const TMDB_API_KEY = "da2b03ca0b0a10e22f32080f94056b75"
+const TMDB_API_KEY = "da2b03ca0b0a10e22f32080f94056b75";
 
 export const getMovieDetails = (name: string): Promise<any> => {
-    const nameToSearch = name.replaceAll(" ", "+").replaceAll("_", "+").split('.').slice(0, -1).join('.')
+  const nameToSearch = name
+    .replaceAll(" ", "+")
+    .replaceAll("_", "+")
+    .split(".")
+    .slice(0, -1)
+    .join(".");
 
+  const response = fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${nameToSearch}`
+  )
+    .then((res) => res.json())
+    // TODO: get best results
+    .catch((error) => console.log(error));
+  return response;
+};
 
-    const response = fetch(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${nameToSearch}`)
-        .then(res => res.json())
-        // TODO: get best results
-        .catch(error => console.log(error))
-    return response
-}
+export const getImage = (path: string) => {
+  return fetch(`https://image.tmdb.org/t/p/w500${path}`)
+    .then((res) => res.blob())
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => console.log(error));
+};
 
 export const getMoviePoster = (id: number) => {
-    return fetch(`https://api.themoviedb.org/3/movie/${id}/images?api_key=${TMDB_API_KEY}&language=en-US&include_image_language=en`)
-        .then(res => res.json())
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/images?api_key=${TMDB_API_KEY}&language=en-US&include_image_language=en`
+  )
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      // get best poster path
+      let path;
+      let bestRating = 0;
+      result.posters?.forEach((poster: any) => {
+        if (poster.vote_average > bestRating) path = poster.file_path;
+      });
+      // TODO: return placeholder image if no poster is found
+      // get image
+      return fetch(`https://image.tmdb.org/t/p/w500${path}`)
+        .then((res) => res.blob())
         .then((result) => {
-            // get best poster path
-            let path;
-            let bestRating = 0;
-            result.posters?.forEach((poster: any) => {
-                if (poster.vote_average > bestRating) path = poster.file_path
-            })
-            // TODO: return placeholder image if no poster is found
-            // get image
-            return fetch(`https://image.tmdb.org/t/p/w500${path}`)
-                .then(res => res.blob())
-                .then((result) => { return result })
-        })
-        .catch(error => console.log(error))
-}
+          return result;
+        });
+    })
+    .catch((error) => console.log(error));
+};
 
 export const getMovieBackdrop = (id: number) => {
-    return fetch(`https://api.themoviedb.org/3/movie/${id}/images?api_key=${TMDB_API_KEY}&language=en-US&include_image_language=en`)
-        .then(res => res.json())
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/images?api_key=${TMDB_API_KEY}&language=en-US&include_image_language=en`
+  )
+    .then((res) => res.json())
+    .then((result) => {
+      // get best backdrop path
+      let path;
+      let bestRating = 0;
+      result.backdrops?.forEach((backdrop: any) => {
+        if (backdrop.vote_average > bestRating) path = backdrop.file_path;
+      });
+      // TODO: return placeholder image if no backdrop is found
+      // get image
+      return fetch(`https://image.tmdb.org/t/p/w500${path}`)
+        .then((res) => res.blob())
         .then((result) => {
-            // get best backdrop path
-            let path;
-            let bestRating = 0;
-            result.backdrops?.forEach((backdrop: any) => {
-                if (backdrop.vote_average > bestRating) path = backdrop.file_path
-            })
-            // TODO: return placeholder image if no backdrop is found
-            // get image
-            return fetch(`https://image.tmdb.org/t/p/w500${path}`)
-                .then(res => res.blob())
-                .then((result) => { return result })
-        })
-        .catch(error => console.log(error))
-}
+          return result;
+        });
+    })
+    .catch((error) => console.log(error));
+};
